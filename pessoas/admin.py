bb125ed0332cc172285220c291a275a7e4f47a5f
grbalmeida <g.r.almeida@live.com>
2022-12-05 20:46:18 -0300

    Ordenação e edição no admin

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -5,6 +5,6 @@ class ListandoPessoas(admin.ModelAdmin):
     list_display = ('id', 'nome', 'email')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
-    list_per_page = 2
+    list_per_page = 5
 
 admin.site.register(Pessoa, ListandoPessoas)
