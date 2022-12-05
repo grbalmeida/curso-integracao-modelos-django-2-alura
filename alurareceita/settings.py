0ffd00a7c700d6fd24066f1712ec027fe2bac725
grbalmeida <g.r.almeida@live.com>
2022-12-05 20:56:28 -0300

    Foto para cada receita

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -127,3 +127,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'alurareceita/static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- receitas/models.py
+++ receitas/models.py
@@ -11,4 +11,5 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now(), blank=True)
+    foto_receita = models.ImageField(upload_to='fotos/%d/%m/%Y/', blank=True)
     publicada = models.BooleanField(default=False)
