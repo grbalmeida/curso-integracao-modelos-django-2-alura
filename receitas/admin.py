3f199e1e48bb69be4d8c644772580e07f842f3fc
grbalmeida <g.r.almeida@live.com>
2022-12-05 20:07:01 -0300

    Busca, Filtros e paginação

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,5 +4,8 @@ from .models import Receita
 class ListandoReceitas(admin.ModelAdmin):
     list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
     list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',)
+    list_filter = ('categoria',)
+    list_per_page = 2
 
 admin.site.register(Receita, ListandoReceitas)
